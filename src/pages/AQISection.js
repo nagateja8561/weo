118d960a276b2c17bf71080fbe6242cb7bc5d62c
Nagateja <naga.teja@orkestrascs.com>
2025-03-26 20:52:25 +0530

    updated colour

--- src/pages/AQISection.js
+++ src/pages/AQISection.js
@@ -62,7 +62,7 @@ const AQISection = ({ view }) => {
   return (
     <div className="max-w-7xl mx-auto p-4 bg-white shadow-lg rounded-lg border-t-4 border-gradient-to-r from-green-400 to-blue-500">
       {/* Full Page Heading */}
-      <h1 className="text-3xl font-extrabold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-red-700 text-center shadow-md">
+      <h1 className="text-3xl font-extrabold mb-8 text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 text-center shadow-md">
         {view === "home"
           ? "Top 4 Polluted Cities in Telangana"
           : ""}
